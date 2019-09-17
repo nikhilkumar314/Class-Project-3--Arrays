53d462aa782dbf3644f65ba4a46d7ede3c7c1d29
Nikhil Kumar <nikhilkumar@usf.edu>
2019-09-17 13:04:30 -0400

    Arrays

--- Class Project 3- Arrays/Program.cs
+++ Class Project 3- Arrays/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/* 
+Author: Nikhil Kumar
+Date: September 17, 2019
+Description: Arrays 
+*/
+
+using System;
 
 namespace Class_Project_3__Arrays
 {
@@ -10,6 +12,18 @@ namespace Class_Project_3__Arrays
     {
         static void Main(string[] args)
         {
+
+            // An array with 25 elements in it
+            int[] Array = new int[25];
+
+            // Iterate through the array with a Foreach Loop
+            for (int i = 0; i < 26; i++)
+            { 
+                //Console writes out some information to the user and displays each iteration of the element in the array
+                Console.WriteLine("Element value = " + i);
+                Console.WriteLine("Iterating through the array..." + "\n" + "Next element loading...");
+            }
         }
     }
 }
+
